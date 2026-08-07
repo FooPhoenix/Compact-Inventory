3184cf33d4e6ca7fff3329c8c0ec5a75f4936618
FooPhoenix <44536562+FooPhoenix@users.noreply.github.com>
2026-08-07 23:32:34 +0200

    Add inventory toggle shortcut prototype

---
+++ compact-inventory_0.1.0/data.lua
@@ -0,0 +1,21 @@
+data:extend({
+    {
+        type = "shortcut",
+        name = "compact-inventory-toggle",
+        action = "lua",
+        toggleable = true,
+        localised_name = "Compact inventory",
+        icons = {
+            {
+                icon = "__base__/graphics/icons/signal/signal_I.png",
+                icon_size = 64
+            }
+        },
+        small_icons = {
+            {
+                icon = "__base__/graphics/icons/signal/signal_I.png",
+                icon_size = 64
+            }
+        }
+    }
+})
