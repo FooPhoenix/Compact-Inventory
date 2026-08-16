8281c7fde315935e9f9ae26774e5acf2981138db
FooPhoenix <44536562+FooPhoenix@users.noreply.github.com>
2026-08-16 20:05:47 +0200

    very small fix

--- compact-inventory_0.1.0/inventory/inventory_source.lua
+++ compact-inventory_0.1.0/inventory/inventory_source.lua
@@ -10,6 +10,9 @@
 ---
 --- ### This class groups all functions used to manage an inventory source.
 ---
+--- @field private lua_inventories LuaInventory[]       The LuaInventory contained in the source.
+---
+--
 local metatable = { }
 
 -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
@@ -94,18 +97,20 @@ function factory.new(...)
     local lua_inventory_count = select("#", ...)
     local lua_inventories     = { ... }
 
+    local source = {                                     ---@type InventorySource
+        lua_inventories = { }
+    }
+
     for index = 1, lua_inventory_count do
         local lua_inventory = lua_inventories[index]
 
-        assert(lua_inventory ~= nil, "InventorySource cannot contain nil LuaInventory !")                              -- [DEBUG-ONLY] . --
+        assert(lua_inventory ~= nil, "InventorySource cannot contain nil LuaInventory !")                                            -- [DEBUG-ONLY] . --
         assert(type(lua_inventory) == "table" or type(lua_inventory) == "userdata", "InventorySource can only contain LuaObject !")  -- [DEBUG-ONLY] . --
-        assert(lua_inventory.valid, "InventorySource can only contain valid LuaInventory !")                            -- [DEBUG-ONLY] . --
-        assert(lua_inventory.object_name == "LuaInventory", "InventorySource can only contain LuaInventory !")         -- [DEBUG-ONLY] . --
+        assert(lua_inventory.valid, "InventorySource can only contain valid LuaInventory !")                                         -- [DEBUG-ONLY] . --
+        assert(lua_inventory.object_name == "LuaInventory", "InventorySource can only contain LuaInventory !")                       -- [DEBUG-ONLY] . --
+    
+        source.lua_inventories[index] = lua_inventory
     end
-
-    local source = {                                     ---@type InventorySource
-        lua_inventories = lua_inventories
-    }
 
     setmetatable(source, metatable)
 
