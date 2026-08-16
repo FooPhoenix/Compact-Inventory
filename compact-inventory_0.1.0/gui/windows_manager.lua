142c804c2ee8dac5708ba0f0d8ff47a80ea0e9fc
FooPhoenix <44536562+FooPhoenix@users.noreply.github.com>
2026-08-16 20:54:07 +0200

    Update window class references

--- compact-inventory_0.1.0/gui/windows_manager.lua
+++ compact-inventory_0.1.0/gui/windows_manager.lua
@@ -3,7 +3,7 @@
 
 -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
 
-local MainInventoryWindowFactory = require("gui.main_inventory")
+local InventoryWindowFactory = require("gui.inventory_window")
 
 -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
 
@@ -15,7 +15,7 @@ local MainInventoryWindowFactory = require("gui.main_inventory")
 --
 local manager = {
     exposed_gui_names = {
-        MainInventoryWindow = MainInventoryWindowFactory.exposed_gui_names
+        InventoryWindow = InventoryWindowFactory.exposed_gui_names
     }
 }
 
@@ -29,27 +29,27 @@ function manager.initialize()
     for _, player in pairs(game.players) do
         manager.initializePlayer(player)
     end
-    
+
 end
 
 -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
 
 function manager.initializePlayer(player)
 
-    MainInventoryWindowFactory.create(player)
+    InventoryWindowFactory.create(player)
 
 end
 
 -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
 
 function manager.getWindowMainInventory(player)
-   
+
     local player_index, player = resolve_player(player)
-    
-    assert(storage.windows.main_inventory[player_index], "Player does not have a main inventory window !")                                          -- [DEBUG-ONLY] . --
-    assert(storage.windows.main_inventory[player_index].valid, "Player does not have a main inventory window !")                                    -- [DEBUG-ONLY] . --
-    assert(storage.windows.main_inventory[player_index].object_name == "LuaMainInventoryWindow", "Player does not have a main inventory window !")  -- [DEBUG-ONLY] . --
-    
+
+    assert(storage.windows.main_inventory[player_index], "Player does not have a main inventory window !")                                 -- [DEBUG-ONLY] . --
+    assert(storage.windows.main_inventory[player_index].valid, "Player does not have a main inventory window !")                           -- [DEBUG-ONLY] . --
+    assert(storage.windows.main_inventory[player_index].object_name == "InventoryWindow", "Player does not have a valid inventory window !")  -- [DEBUG-ONLY] . --
+
     return storage.windows.main_inventory[player_index]
 end
 
