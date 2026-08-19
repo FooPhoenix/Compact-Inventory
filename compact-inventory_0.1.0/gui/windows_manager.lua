3f3e3a537147d9fca7d861b12867236e849069d0
FooPhoenix <44536562+FooPhoenix@users.noreply.github.com>
2026-08-19 09:23:53 +0200

    Support recreating the main inventory window

--- compact-inventory_0.1.0/gui/windows_manager.lua
+++ compact-inventory_0.1.0/gui/windows_manager.lua
@@ -51,6 +51,52 @@ end
 
 -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
 
+function manager.hasWindowMainInventory(player)
+
+    local player_index = resolve_player(player)
+    local window = storage.windows.main_inventory[player_index]
+
+    return window ~= nil and window.valid
+end
+
+-- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
+
+function manager.createWindowMainInventory(player)
+
+    local player_index, lua_player = resolve_player(player)
+
+    assert(not manager.hasWindowMainInventory(lua_player), "Player already has a main inventory window !")      -- [DEBUG-ONLY] . --
+
+    if storage.windows.main_inventory[player_index] then
+        storage.windows.main_inventory[player_index] = nil
+    end
+
+    local lua_inventory = lua_player.get_main_inventory()
+
+    assert(lua_inventory and lua_inventory.valid, "Player must have a valid main LuaInventory !")      -- [DEBUG-ONLY] . --
+
+    local inventory
+
+    for _, monitored_inventory in pairs(InventoryManagerFactory.get(lua_player):getInventories()) do
+        if monitored_inventory:getSource():containsInventory(lua_inventory) then
+            assert(inventory == nil, "Several monitored Inventory contain the player's main LuaInventory !")      -- [DEBUG-ONLY] . --
+            inventory = monitored_inventory
+        end
+    end
+
+    assert(inventory, "No monitored Inventory contains the player's main LuaInventory !")      -- [DEBUG-ONLY] . --
+
+    return InventoryWindowFactory.create(lua_player, inventory)
+end
+
+-- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
+
+function manager.destroyWindowMainInventory(player)
+    InventoryWindowFactory.destroy(player)
+end
+
+-- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
+
 function manager.getWindowMainInventory(player)
 
     local player_index = resolve_player(player)
