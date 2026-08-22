9756b8ce590f4e445d0bf345d21fc3e03fa1a3b7
FooPhoenix <44536562+FooPhoenix@users.noreply.github.com>
2026-08-22 09:05:28 +0200

    Shrink tree rename icon

--- compact-inventory_0.1.0/data.lua
+++ compact-inventory_0.1.0/data.lua
@@ -82,7 +82,7 @@ data:extend({
         filename = "__compact-inventory__/graphics/rename-icon-white.png",
         size     = 32,
         flags    = { "gui-icon" },
-        scale    = 0.5
+        scale    = 0.4375
     },
     {
         type     = "sprite",
