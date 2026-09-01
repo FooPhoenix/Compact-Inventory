6eda310fb96870f47a6e3274c94ed5fa6b4415fc
FooPhoenix <44536562+FooPhoenix@users.noreply.github.com>
2026-09-01 23:17:41 +0200

    Add abstract inventory source types

---
+++ compact-inventory/inventory/inventory_type.lua
@@ -0,0 +1,20 @@
+local InventoryType = {
+    character_main  = "character_main",
+    character_ammo  = "character_ammo",
+    character_guns  = "character_guns",
+    character_armor = "character_armor",
+    character_trash = "character_trash",
+
+    vehicle_main  = "vehicle_main",
+    vehicle_ammo  = "vehicle_ammo",
+    vehicle_trash = "vehicle_trash",
+    vehicle_fuel  = "vehicle_fuel",
+
+    chest_main  = "chest_main",
+    chest_trash = "chest_trash",
+
+    train_cargo = "train_cargo",
+    train_fuel  = "train_fuel"
+}
+
+return InventoryType
