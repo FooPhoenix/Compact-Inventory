69d2970d4c57e1de633761b1acfbe6f72c20941d
FooPhoenix <44536562+FooPhoenix@users.noreply.github.com>
2026-09-01 23:19:54 +0200

    Align static inventory types with Factorio IDs

--- compact-inventory/inventory/inventory_type.lua
+++ compact-inventory/inventory/inventory_type.lua
@@ -1,17 +1,17 @@
 local InventoryType = {
-    character_main  = "character_main",
-    character_ammo  = "character_ammo",
-    character_guns  = "character_guns",
-    character_armor = "character_armor",
-    character_trash = "character_trash",
+    character_main  = defines.inventory.character_main,
+    character_ammo  = defines.inventory.character_ammo,
+    character_guns  = defines.inventory.character_guns,
+    character_armor = defines.inventory.character_armor,
+    character_trash = defines.inventory.character_trash,
 
     vehicle_main  = "vehicle_main",
     vehicle_ammo  = "vehicle_ammo",
     vehicle_trash = "vehicle_trash",
     vehicle_fuel  = "vehicle_fuel",
 
-    chest_main  = "chest_main",
-    chest_trash = "chest_trash",
+    chest_main  = defines.inventory.chest,
+    chest_trash = defines.inventory.logistic_container_trash,
 
     train_cargo = "train_cargo",
     train_fuel  = "train_fuel"
