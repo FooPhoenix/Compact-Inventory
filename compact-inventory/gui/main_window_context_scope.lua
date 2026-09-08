977477a4aad23ad269ec263d31fb60c3b96d8499
FooPhoenix <44536562+FooPhoenix@users.noreply.github.com>
2026-09-08 14:57:39 +0200

    Add main window context scopes

---
+++ compact-inventory/gui/main_window_context_scope.lua
@@ -0,0 +1,7 @@
+local ContextScope = {
+    working_state      = "working_state",
+    source_selector    = "source_selector",
+    inventory_selector = "inventory_selector"
+}
+
+return ContextScope
